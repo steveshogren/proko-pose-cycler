ef9e6d76882ca3a36c8a9b6f8ffac8039162a7da
Steve Shogren <madklowns@gmail.com>
2014-11-04 20:50:55 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,48 +1,70 @@
 var seconds_left = 30;
+var session_length = 30;
+var pause = false;
 var rand = function(s, e) {
     return Math.floor((Math.random() * (e-s)) + s);
 };
 var times = 1;
 var pad = function (str, max) {
-    str = str.toString();
-    return str.length < max ? pad("0" + str, max) : str;
-};
-var pickRandomImage = function() {
-    var which = rand(0,5);
-    var t = [{ file : ".\\Poses\\Chanon_Large\\Chanon",
-               max : 313},
-             { file : ".\\Poses\\Aaron_Large\\Aaron" ,
-               max : 325},
-             {file : ".\\Poses\\Marcia_Large\\Marcia",
-              max : 226},
-             {file : ".\\Poses\\Veronica_Large\\Veronica",
-              max : 433},
-             {file : ".\\Poses\\Yoni_Large\\Yoni",
-              max : 306}];
-    return t[which];
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
+}
+
+var percentOver = function () {
+    return (seconds_left/session_length)*100;
 };
 var randomImage = function() {
-    var image = pickRandomImage();
-    var file = image.file;
-    var max = image.max;
-    document.getElementById('image').src = file + pad(rand(1, max),3) + ".jpg";
+    var which = rand(1,6);
+    var file = "";
+    var max = 1;
+    if (which == 1) {
+        file = ".\\Poses\\Chanon_Large\\Chanon" ;
+        max = 313;
+    }
+    if (which == 2) {
+        file = ".\\Poses\\Aaron_Large\\Aaron" ;
+        max = 325;
+    }
+    if (which == 3) {
+        file = ".\\Poses\\Marcia_Large\\Marcia" ;
+        max = 226;
+    }
+    if (which == 4) {
+        file = ".\\Poses\\Veronica_Large\\Veronica" ;
+        max = 433;
+    }
+    if (which == 5) {
+        file = ".\\Poses\\Yoni_Large\\Yoni" ;
+        max = 306;
+    }
+    document.getElementById('image').src = file + pad(rand(1, max),3) + ".jpg" 
 };
 randomImage();
-var interval = setInterval(function() {
-    seconds_left--;
+
+var cycleFn = function() {
+    if (! pause) {
+        seconds_left--;
+    }
     document.getElementById('timer_div').innerHTML = pad(seconds_left, 3) + " - " + pad(times,2);
+    document.getElementById('progressBar').style.width = percentOver() + "%";
     if (seconds_left <= 0) {
         randomImage();
-
-        if (times <= 10) {seconds_left = 30; }
-        else if (times <= 14) {seconds_left = 60; }
-        else if (times <= 16) {seconds_left = 300; }
-        else {seconds_left = 600; }
+        
+        if (times <= 10) {seconds_left = 30; session_length = 30; }
+        else if (times <= 14) {seconds_left = 60; session_length = 60; }
+        else if (times <= 16) {seconds_left = 300; session_length = 300; }
+        else {seconds_left = 600; session_length = 600;}
         times++;
+    }
+};
+                           
+var interval = setInterval(cycleFn, 1000);
 
-    }}
-                           , 1000);
+var togglePause = function() {
+    pause = !pause;
+};
 
 var skip = function() {
+    pause = false;
     seconds_left = 1;
 };
